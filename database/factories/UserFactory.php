280ff7fe2a0ef821f20e0d8a2983b50bcae01688
asenadilan <asenadilancamak@gmail.com>
2021-07-13 12:39:20 +0300

    quiz migration oluşturuldu factoryseed oluşturuldu seed yapısı tablolara bölündü

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Features;
+use Illuminate\Support\Facades\DB;
 
 class UserFactory extends Factory
 {
@@ -24,9 +25,20 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        DB::table("users")->truncate();
+        User::insert([
+            'name' => "Furkan Gürel" ,
+            'email' => "asenadilancamak@gmail.com",
+            "type" => "admin",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        $types=["admin","user"];
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            "type" => $types[rand(0,1)],
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
